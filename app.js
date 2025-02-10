6e11f3fba1c190cdccef406cc62855ae8923a570
kirnic <marcelokirnic@gmail.com>
2025-02-10 16:42:55 -0300

    migrated db

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require("bcryptjs")
+const assetsPath = path.join(__dirname, "public");
+
 const pool = new Pool({
   // add your configuration
   user: process.env.PGUSER,
@@ -20,6 +22,7 @@ const pool = new Pool({
 });
 
 const app = express();
+app.use(express.static(assetsPath));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -29,7 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
     res.render("index", { user: req.user });
   });
-  
+  app.use(express.static(assetsPath));
 
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.post(
@@ -41,31 +44,33 @@ app.post(
   );
   
 
-app.post("/sign-up", async (req, res, next) => {
-  try {
-    await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-      req.body.username,
-      req.body.password,
-    ]);
-    res.redirect("/");
-  } catch(err) {
-    return next(err);
-  }
-});
-
+  app.post("/sign-up", async (req, res, next) => {
+    try {
+     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+     await pool.query("insert into onlymember (username, password,firstname,secondname,ismember) values ($1, $2,$3,$4,$5)", [req.body.username, hashedPassword,req.body.firstname,req.body.secondname,ismember = false]);
+     res.redirect("/");
+    } catch (error) {
+       console.error(error);
+       next(error);
+      }
+   });
+   
   
 passport.use(
     new LocalStrategy(async (username, password, done) => {
       try {
-        const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+        const { rows } = await pool.query("SELECT * FROM onlymember WHERE username = $1", [username]);
         const user = rows[0];
   
         if (!user) {
           return done(null, false, { message: "Incorrect username" });
         }
-        if (user.password !== password) {
-          return done(null, false, { message: "Incorrect password" });
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" })
         }
+        
         return done(null, user);
       } catch(err) {
         return done(err);
@@ -79,7 +84,7 @@ passport.use(
   
   passport.deserializeUser(async (id, done) => {
     try {
-      const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
+      const { rows } = await pool.query("SELECT * FROM onlymember WHERE id = $1", [id]);
       const user = rows[0];
   
       done(null, user);
@@ -97,5 +102,17 @@ passport.use(
       res.redirect("/");
     });
   });
-  
+  app.get("/member",(req,res)=>{
+   
+    res.render("member",{user:req.user})
+  })
+  app.post("/member",async (req,res)=>  {
+    const id = req.user.id
+    
+    await pool.query("UPDATE onlymember SET ismember = true WHERE id =$1",[id])
+    
+    
+    res.redirect("/")
+
+  }) 
 app.listen(3000, () => console.log("app listening on port 3000!"));
